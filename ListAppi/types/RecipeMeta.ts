d635410843d9853cf92cce4a365599f46b2b69ec
Jemina Patrikainen <jemina.patrikainen@gmail.com>
2026-01-17 10:41:34 +0200

    Reseptien suodattimet lisätty

---
+++ ListAppi/types/RecipeMeta.ts
@@ -0,0 +1,25 @@
+export type MealType =
+    | 'aamupala'
+    | 'lounas'
+    | 'päivällinen'
+    | 'välipala'
+    | 'jälkiruoka'
+    | 'alkupala'
+    | 'juoma'
+    | 'lisuke'
+
+export type MainIngredient =
+    | 'kana'
+    | 'liha'
+    | 'kala'
+    | 'kasvis'
+    | 'pasta'
+    | 'riisi'
+    | 'muu'
+
+export type DietType =
+    | 'vegaaninen'
+    | 'laktoositon'
+    | 'gluteeniton'
+    | 'maidoton'
+    | 'kasvisruoka'
